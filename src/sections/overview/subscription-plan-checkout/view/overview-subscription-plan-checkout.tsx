92f8978f9b9e3c6fe31d513dd48cec009a38d88e
Muhammad Hamad <42932321+Hamaddogar@users.noreply.github.com>
2024-02-11 11:46:12 +0500

    update changes

--- src/sections/overview/subscription-plan-checkout/view/overview-subscription-plan-checkout.tsx
+++ src/sections/overview/subscription-plan-checkout/view/overview-subscription-plan-checkout.tsx
@@ -11,7 +11,6 @@ import Radio from '@mui/material/Radio';
 import { useSettingsContext } from 'src/components/settings';
 import { Paper, Typography, Grid } from '@mui/material';
 import { Box, Stack } from '@mui/system';
-import { ConfirmDialog } from 'src/components/custom-dialog';
 import React from 'react';
 import { paths } from 'src/routes/paths';
 import CustomCrumbs from 'src/components/custom-crumbs/custom-crumbs';
@@ -22,19 +21,11 @@ import Linker from '../link';
 export default function OverviewSubscriptionPlanCheckout() {
   const settings = useSettingsContext();
   const [checked, setchecked] = React.useState('visa-mastercard');
-  const [open, setOpen] = React.useState(false);
-
-  const handleToggle = () => {
-    setOpen((pv) => !pv);
-  };
 
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
-      <Grid
-        container
-        justifyContent="space-between"
-        alignItems={{ xs: 'flex-start', md: 'center' }}
-      >
+
+      <Grid container justifyContent='space-between' alignItems={{ xs: 'flex-start', md: 'center' }}>
         <Grid item xs={12} md="auto">
           <CustomCrumbs
             heading="Checkout"
@@ -44,250 +35,117 @@ export default function OverviewSubscriptionPlanCheckout() {
         </Grid>
 
         <Grid item xs={12} md={5}>
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent={{ xs: 'flex-start', md: 'flex-end' }}
-            spacing="20px"
-          >
+          <Stack direction='row' alignItems='center' justifyContent={{ xs: 'flex-start', md: 'flex-end' }} spacing='20px'>
             <Linker path={paths.dashboard.general.subscriptionplan}>
-              <Button
-                fullWidth
-                sx={{
-                  minWidth: '110px !important',
-                  borderRadius: '30px',
-                  color: '#8688A3',
-                  backgroundColor: '#F0F0F4',
-                }}
-                component="h5"
-                variant="contained"
-              >
-                {' '}
-                Cancel{' '}
-              </Button>
+              <Button fullWidth sx={{ minWidth: '110px !important', borderRadius: '30px', color: '#8688A3', backgroundColor: '#F0F0F4' }} component='h5' variant='contained'> Cancel </Button>
             </Linker>
-            <Box>
-              <Button
-                onClick={handleToggle}
-                fullWidth
-                sx={{ minWidth: '110px !important', borderRadius: '30px', color: '#0F1349' }}
-                component="h5"
-                variant="contained"
-                color="primary"
-              >
-                {' '}
-                Pay{' '}
-              </Button>
-            </Box>
+
+            <Linker path={paths.dashboard.root}>
+              <Button fullWidth sx={{ minWidth: '110px !important', borderRadius: '30px', color: '#0F1349' }} component='h5' variant='contained' color='primary'> Pay </Button>
+            </Linker>
           </Stack>
         </Grid>
       </Grid>
 
-      <Box sx={{ margin: '30px 0px' }}>
-        {' '}
-        <Divider />{' '}
-      </Box>
+      <Box sx={{ margin: '30px 0px' }}>  <Divider />  </Box>
 
-      <Grid container justifyContent="center" spacing="25px">
+      <Grid container justifyContent='center' spacing='25px'>
+
+
+
+
         <Grid item xs={12} md={6}>
-          <Typography variant="h5" mb="30px" sx={{ whiteSpace: 'pre-line', fontSize: '1rem' }}>
-            {' '}
-            Select Payment Method{' '}
-          </Typography>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-            sx={{
-              width: '100%',
-              height: '60px',
-              border: checked === 'visa-mastercard' ? '2px solid #1BFCB6' : '2px solid #E6E6EC',
-              background:
-                checked === 'visa-mastercard' ? 'rgb(27, 252, 182,.3)' : 'rgb(27, 252, 182,0)',
-              borderRadius: '16px',
-              padding: '0px 24px',
-              mb: '15px',
-              transition: 'all .4s',
-            }}
-            onClick={() => setchecked('visa-mastercard')}
+          <Typography variant="h5" mb="30px" sx={{ whiteSpace: 'pre-line', fontSize: '1rem' }}>  Select Payment Method  </Typography>
+          <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{
+            width: '100%',
+            height: '60px',
+            border: checked === "visa-mastercard" ? "2px solid #1BFCB6" : "2px solid #E6E6EC",
+            background: checked === "visa-mastercard" ? "rgb(27, 252, 182,.3)" : "rgb(27, 252, 182,0)",
+            borderRadius: '16px',
+            padding: '0px 24px',
+            mb: '15px',
+            transition: 'all .4s'
+          }}
+            onClick={() => setchecked("visa-mastercard")}
           >
-            <FormControlLabel
-              value="visa-mastercard"
-              control={<Radio checked={checked === 'visa-mastercard'} size="medium" />}
-              label="Payment Card"
-            />
-            <Box component="img" src="/raw/visa-mastercard.svg" />
+            <FormControlLabel value="visa-mastercard" control={<Radio checked={checked === "visa-mastercard"} size="medium" />} label="Payment Card" />
+            <Box component='img' src='/raw/visa-mastercard.svg' />
           </Stack>
 
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-            sx={{
-              width: '100%',
-              height: '60px',
-              border: checked === 'KNET' ? '2px solid #1BFCB6' : '2px solid #E6E6EC',
-              background: checked === 'KNET' ? 'rgb(27, 252, 182,.3)' : 'rgb(27, 252, 182,0)',
-              borderRadius: '16px',
-              padding: '0px 24px',
-              transition: 'all .4s',
-            }}
-            onClick={() => setchecked('KNET')}
+
+          <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{
+            width: '100%',
+            height: '60px',
+            border: checked === "KNET" ? "2px solid #1BFCB6" : "2px solid #E6E6EC",
+            background: checked === "KNET" ? "rgb(27, 252, 182,.3)" : "rgb(27, 252, 182,0)",
+            borderRadius: '16px',
+            padding: '0px 24px',
+            transition: 'all .4s'
+          }}
+            onClick={() => setchecked("KNET")}
           >
-            <FormControlLabel
-              value="KNET"
-              control={<Radio checked={checked === 'KNET'} size="medium" />}
-              label="Payment Card"
-            />
-            <Box component="img" src="/raw/KNET.svg" />
+            <FormControlLabel value="KNET" control={<Radio checked={checked === "KNET"} size="medium" />} label="Payment Card" />
+            <Box component='img' src='/raw/KNET.svg' />
           </Stack>
+
+
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={10} sx={{ padding: '20px', borderRadius: '12px' }}>
-            <Typography variant="h5" mb="30px" sx={{ whiteSpace: 'pre-line', fontSize: '1rem' }}>
-              {' '}
-              Summary{' '}
-            </Typography>
+          <Paper elevation={10} sx={{ padding: '20px', borderRadius: '12px' }} >
+            <Typography variant="h5" mb="30px" sx={{ whiteSpace: 'pre-line', fontSize: '1rem' }}>  Summary </Typography>
 
-            <Stack direction="row" alignItems="center" justifyContent="space-between">
-              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>
-                {' '}
-                Plan Name{' '}
-              </Typography>
-              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>
-                {' '}
-                Pro Plan{' '}
-              </Typography>
+            <Stack direction='row' alignItems='center' justifyContent='space-between'>
+              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>  Plan Name </Typography>
+              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>  Pro Plan </Typography>
             </Stack>
 
-            <Box sx={{ margin: '20px 0px' }}>
-              {' '}
-              <Divider />{' '}
-            </Box>
+            <Box sx={{ margin: '20px 0px' }}>  <Divider />  </Box>
 
-            <Stack direction="row" alignItems="center" justifyContent="space-between">
-              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>
-                {' '}
-                Subscription{' '}
-              </Typography>
-              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>
-                {' '}
-                12 months{' '}
-              </Typography>
+
+            <Stack direction='row' alignItems='center' justifyContent='space-between'>
+              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>  Subscription </Typography>
+              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>  12 months </Typography>
             </Stack>
-            <Stack mt="15px" direction="row" alignItems="center" justifyContent="space-between">
-              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>
-                {' '}
-                From{' '}
-              </Typography>
-              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>
-                {' '}
-                10 June, 2023{' '}
-              </Typography>
+            <Stack mt='15px' direction='row' alignItems='center' justifyContent='space-between'>
+              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>  From </Typography>
+              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>  10 June, 2023 </Typography>
             </Stack>
-            <Stack mt="15px" direction="row" alignItems="center" justifyContent="space-between">
-              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>
-                {' '}
-                To{' '}
-              </Typography>
-              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>
-                {' '}
-                9 June, 2024{' '}
-              </Typography>
+            <Stack mt='15px' direction='row' alignItems='center' justifyContent='space-between'>
+              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>  To </Typography>
+              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>  9 June, 2024 </Typography>
             </Stack>
 
-            <Box sx={{ margin: '20px 0px' }}>
-              {' '}
-              <Divider />{' '}
-            </Box>
 
-            <Stack direction="row" alignItems="center" justifyContent="space-between">
-              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>
-                {' '}
-                Subscription Fees{' '}
-              </Typography>
-              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>
-                {' '}
-                120 KWD{' '}
-              </Typography>
+
+
+            <Box sx={{ margin: '20px 0px' }}>  <Divider />  </Box>
+
+
+            <Stack direction='row' alignItems='center' justifyContent='space-between'>
+              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>  Subscription Fees </Typography>
+              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>  120 KWD </Typography>
             </Stack>
-            <Stack mt="15px" direction="row" alignItems="center" justifyContent="space-between">
-              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>
-                {' '}
-                Discount{' '}
-              </Typography>
-              <Typography variant="subtitle1" color="error" sx={{ fontSize: '1rem' }}>
-                {' '}
-                - 20 KWD
-              </Typography>
+            <Stack mt='15px' direction='row' alignItems='center' justifyContent='space-between'>
+              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>  Discount </Typography>
+              <Typography variant="subtitle1" color='error' sx={{ fontSize: '1rem' }}>  - 20 KWD</Typography>
             </Stack>
-            <Box sx={{ margin: '20px 0px' }}>
-              {' '}
-              <Divider />{' '}
-            </Box>
-            <Stack direction="row" alignItems="center" justifyContent="space-between">
-              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>
-                {' '}
-                Total{' '}
-              </Typography>
-              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>
-                {' '}
-                100 KWD{' '}
-              </Typography>
+            <Box sx={{ margin: '20px 0px' }}>  <Divider />  </Box>
+            <Stack direction='row' alignItems='center' justifyContent='space-between'>
+              <Typography variant="subtitle1" sx={{ opacity: 0.6, fontSize: '1rem' }}>  Total </Typography>
+              <Typography variant="subtitle1" sx={{ fontSize: '1rem' }}>  100 KWD </Typography>
             </Stack>
+
+
+
+
+
+
           </Paper>
         </Grid>
       </Grid>
-      <ConfirmDialog
-        open={open}
-        onClose={handleToggle}
-        noCancel={false}
-        maxWidth="sm"
-        content={
-          <Box textAlign="center">
-            <Box component="img" src="/raw/Confetti_L.svg" alt="" sx={{ width: '100%' }} />
-            <Box component="img" src="/raw/happy.svg" alt="" sx={{ mt: '-8%' }} />
 
-            <Typography
-              component="p"
-              variant="h4"
-              sx={{ maxWidth: '300px', marginX: 'auto', mt: '26px' }}
-            >
-              Payment Successful!
-            </Typography>
 
-            <Typography
-              component="p"
-              variant="body1"
-              sx={{ mt: '12px', color: '#8688A3', maxWidth: '412px', marginX: 'auto' }}
-            >
-              The payment has been done successfully.
-              <br />
-              Thanks for being there with us
-            </Typography>
-
-            <Box mt="20px">
-              <Linker path={paths.dashboard.root} width="100%">
-                <Button
-                  variant="contained"
-                  size="large"
-                  color="primary"
-                  fullWidth
-                  sx={{
-                    color: '#0F1349',
-                    borderRadius: '30px',
-                    boxShadow: '0px 6px 20px #1BFCB633',
-                  }}
-                  onClick={handleToggle}
-                >
-                  Go Back
-                </Button>
-              </Linker>
-            </Box>
-          </Box>
-        }
-      />
-    </Container>
+    </Container >
   );
 }
